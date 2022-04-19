74fcf5c835d7af78e4932a0dc0728865174942b3
Lena Hou <lenahou2013@gmail.com>
2022-04-18 20:14:31 -0400

    seeding some data

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,14 @@
 class JobsController < ApplicationController
+
+    def index
+        @jobs = Job.all
+        render json: @jobs, key_transform: :camel_lower
+
+    end
+
+    def show
+        @job = Job.find(params[:id])
+        render json: @job, key_transform: :camel_lower
+    end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,81 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Job.create([
+    {
+        client: "PwC",
+        poc: "Maria Rodriguez",
+        email: "M.RD43@datapipertech.com",
+        role: "Core Senior Software Developer - Senior Associate",
+        urgency: "High",
+        quantity: 1,
+        skills_needed: "Java,Springboot Docker/Kubernetes, API/microservices/Cloud"
+    },
+
+    {
+        client: "PwC",
+        poc: "Maria Rodriguez",
+        email: "M.RD43@datapipertech.com",
+        role: "Core Senior Software Developer - Senior",
+        urgency: "High",
+        quantity: 1,
+        skills_needed: "Java,Springboot Docker/Kubernetes, API/microservices/Cloud"
+    },
+
+    {
+        client: "PwC",
+        poc: "David Smith",
+        email: "DavidS_33@datapipertech.com",
+        role: "Zendesk Developer",
+        urgency: "Med",
+        quantity: 1,
+        skills_needed: "Zendesk Developer"
+    },
+
+    {
+        client: "PwC",
+        poc: "Rob Adams",
+        email: "2Robert@datapipertech.com",
+        role: "Datavault 2.0",
+        urgency: "High",
+        quantity: 3,
+        skills_needed: "Datavault 2.0 Certified"
+    },
+
+    {
+        client: "PwC",
+        poc: "Eliza Cath",
+        email: "ElizaT_Cat@datapipertech.com",
+        role: "Snowflake data engineer",
+        urgency: "Low",
+        quantity: 1,
+        skills_needed: "JD"
+    }
+])
+
+Candidate.create([
+    {
+        first_name: "John",
+        last_name: "Doe",
+        email: "johndoe123@gmail.com",
+        phone: "603-771-7211",
+        address: "1097 Milford Street",
+        city: "Manchester",
+        state: "New Hampshire",
+        zipcode: "03103",
+        job_id: 3
+    },
+
+    {
+        first_name: "Fang",
+        last_name: "Smith",
+        email: "fangsmith33@gmail.com",
+        phone: "952-432-2211",
+        address: "1795 Oral Lake Road",
+        city: "Apple Valley",
+        state: "Minnesota",
+        zipcode: "55124",
+        job_id: 1
+    }
+])
