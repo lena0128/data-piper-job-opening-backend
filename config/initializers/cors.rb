97af5c17b67042ba708ee6c2525f3589e65c4b4e
Lena Hou <lenahou2013@gmail.com>
2022-04-20 21:15:29 -0400

    updating cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'https://data-piper-jobs.netlify.app'
+     origins '*'
 
      resource '*',
        headers: :any,
