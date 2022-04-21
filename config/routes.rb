5657826113750f653392e011c38df4fc8b952816
Lena Hou <lenahou2013@gmail.com>
2022-04-20 20:09:18 -0400

    configure the backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'jobs#index' 
+
   resources :candidates
   resources :jobs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
